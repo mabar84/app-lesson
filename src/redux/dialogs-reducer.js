e915486cb31bd6164808a1b3d5734a4001eb207b
Baranouski <mabar@tut.by>
2022-10-12 12:24:59 +0300

    les 48 "stateCopy DialogsReducer" done

--- src/redux/dialogs-reducer.js
+++ src/redux/dialogs-reducer.js
@@ -1,66 +1,63 @@
 const ADD_MESSAGE = "ADD-MESSAGE";
 const UPDATE_NEW_MESSAGE_TEXT = "UPDATE-NEW-MESSAGE-TEXT";
 
-let initialState={
-  dialogsData: [
-    {
-      id: 1,
-      name: "Саша",
-      src: "https://avatarko.ru/img/kartinka/15/film_znamenitost_14478.jpg",
-    },
-    {
-      id: 2,
-      name: "Маша",
-      src: "https://avatarko.ru/img/avatar/16/film_15559.jpg",
-    },
-    {
-      id: 3,
-      name: "Петя",
-      src: "https://avatarko.ru/img/kartinka/2/elf_Middle-earth_Legolas_1952.jpg",
-    },
-    {
-      id: 4,
-      name: "Вася",
-      src: "https://avatarko.ru/img/kartinka/28/film_hobbit_27970.jpg",
-    },
-    {
-      id: 5,
-      name: "Света",
-      src: "https://avatarko.ru/img/kartinka/1/Arwen.jpg",
-    },
-  ],
-  messagesData: [
-    { id: 1, message: "Hi" },
-    { id: 2, message: "How are you?" },
-    { id: 3, message: "Yo" },
-    { id: 4, message: "Yo-yo!!!" },
-  ],
-  newMessageText: "",
+let initialState = {
+    dialogsData: [
+        {
+            id: 1,
+            name: "Саша",
+            src: "https://avatarko.ru/img/kartinka/15/film_znamenitost_14478.jpg",
+        },
+        {
+            id: 2,
+            name: "Маша",
+            src: "https://avatarko.ru/img/avatar/16/film_15559.jpg",
+        },
+        {
+            id: 3,
+            name: "Петя",
+            src: "https://avatarko.ru/img/kartinka/2/elf_Middle-earth_Legolas_1952.jpg",
+        },
+        {
+            id: 4,
+            name: "Вася",
+            src: "https://avatarko.ru/img/kartinka/28/film_hobbit_27970.jpg",
+        },
+        {
+            id: 5,
+            name: "Света",
+            src: "https://avatarko.ru/img/kartinka/1/Arwen.jpg",
+        },
+    ],
+    messagesData: [
+        {id: 1, message: "Hi"},
+        {id: 2, message: "How are you?"},
+        {id: 3, message: "Yo"},
+        {id: 4, message: "Yo-yo!!!"},
+    ],
+    newMessageText: "",
 }
 
-const dialogsReducer = (state=initialState, action) => {
-  switch (action.type) {
-    case ADD_MESSAGE:
-      let newMessage = {
-        id: 5,
-        message: state.newMessageText,
-      };
-      state.messagesData.push(newMessage);
-      state.newMessageText = "";
-      return state;
-    case UPDATE_NEW_MESSAGE_TEXT:
-      state.newMessageText = action.text;
-      return state;
-    default:
-      return state;
-  }
+const dialogsReducer = (state = initialState, action) => {
+    switch (action.type) {
+        case ADD_MESSAGE:
+            return {
+                ...state,
+                newMessageText: "",
+                messagesData: [...state.messagesData, {id: 5, message: state.newMessageText}]
+            }
+        case UPDATE_NEW_MESSAGE_TEXT:
+            return {...state, newMessageText: action.text}
+        default:
+            return state;
+    }
 };
 
-export const sendMessageCreator = () => ({ type: ADD_MESSAGE });
+export const sendMessageCreator = () => ({type: ADD_MESSAGE});
 
 export const updateNewMessageCreator = (text) => ({
-  type: UPDATE_NEW_MESSAGE_TEXT,
-  text: text,
+    type: UPDATE_NEW_MESSAGE_TEXT,
+    text: text,
 });
 
 export default dialogsReducer;
--- src/redux/profile-reducer.js
+++ src/redux/profile-reducer.js
@@ -13,25 +13,14 @@ let initialState = {
 const profileReducer = (state = initialState, action) => {
 
     switch (action.type) {
-        case ADD_POST: {
-            let newPost = {
-                id: 4,
-                message: stateCopy.newPostText,
-                like: 11,
-            };
-
-            let stateCopy = {...state}
-            stateCopy.posts = [...state.posts]
-
-            stateCopy.posts.push(newPost);
-            stateCopy.newPostText = "";
-            return stateCopy
-        }
-        case UPDATE_NEW_POST_TEXT: {
-            let stateCopy = {...state}
-            stateCopy.newPostText = action.text;
-            return stateCopy;
-        }
+        case ADD_POST:
+            return {
+                ...state,
+                newPostText: "",
+                posts: [...state.posts, {id: 4, message: state.newPostText, like: 11}]
+            }
+        case UPDATE_NEW_POST_TEXT:
+            return {...state, newPostText: action.text}
         default:
             return state;
     }
