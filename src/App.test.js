48f5001e84838990cedcc4b9d241d3e899e84e4d
HUAWEI <mabar@tut.by>
2023-01-02 16:07:29 +0300

    les92 'react-test-renderer' is done

--- src/App.test.js
+++ src/App.test.js
@@ -2,9 +2,16 @@ import React from 'react';
 import ReactDom from 'react-dom';
 import ReactDOM from 'react-dom';
 import SocialNetworkApp from './App';
+import {createRoot} from "react-dom/client";
 
 test('renders without crashing', () => {
-    const div = document.createElement('div')
-    ReactDOM.render(<SocialNetworkApp/>, div)
-    ReactDom.unmountComponentAtNode(div)
+    // const div = document.createElement('div')
+    // ReactDOM.render(<SocialNetworkApp/>, div)
+    // ReactDom.unmountComponentAtNode(div)
+
+    const container = document.createElement('div');
+    const root = createRoot(container);
+    root.render(<SocialNetworkApp tab="home" />);
+    root.unmount();
+
 });
--- src/components/Profile/ProfileInfo/ProfileStatus.test.jsx
+++ src/components/Profile/ProfileInfo/ProfileStatus.test.jsx
@@ -1,11 +1,51 @@
 import React from 'react';
-import {create} from 'domain';
 import ProfileStatus from './ProfileStatus';
+import TestRenderer, {create} from "react-test-renderer";
 
+describe('ProfileStatus component', () => {
+    test('status from props should be in the state', () => {
+        const component = TestRenderer.create(<ProfileStatus status={'mabar'}/>)
+        const instance = component.getInstance()
+        expect(instance.state.status).toBe('mabar')
+    })
+    test('after creation span should be displayed', () => {
+        const component = create(<ProfileStatus status={'mabar'}/>)
+        const root = component.root
+        // eslint-disable-next-line testing-library/await-async-query
+        let span = root.findByType("span")
+        expect(span).not.toBeNull()
+    })
+    test(`after creation input shouldn't be displayed`, () => {
+        const component = create(<ProfileStatus status={'mabar'}/>)
+        const root = component.root
+        expect(() => {
+            // eslint-disable-next-line testing-library/await-async-query
+            let input = root.findByType("input")
+        }).toThrow()
+    })
+    test('after creation span should contains correct status', () => {
+        const component = create(<ProfileStatus status={'mabar'}/>)
+        const root = component.root
+        // eslint-disable-next-line testing-library/await-async-query
+        let span = root.findByType("span")
+        expect(span.children[0]).toBe('mabar')
+    })
+    test('input should be displayed in editMode instead span', () => {
+        const component = create(<ProfileStatus status={'mabar'}/>)
+        const root = component.root
+        // eslint-disable-next-line testing-library/await-async-query
+        let span = root.findByType("span")
+        span.props.onDoubleClick()
+        // eslint-disable-next-line testing-library/await-async-query
+        let input = root.findByType("input")
+        expect(input.props.value).toBe('mabar')
+    })
+    test('callback should be called', () => {
+        const mockCallback = jest.fn()
+        const component = create(<ProfileStatus status={'mabar'} updateUserStatus={mockCallback}/>)
+        const instance = component.getInstance()
+        instance.deactivateEditMode()
+        expect(mockCallback.mock.calls.length).toBe(1)
+    })
+})
 
-test('status from props should be in the state', () => {
-   const component = create(<ProfileStatus status={'mabar'}  />)
-    const instance = component.
-    // const instance = component.getInstance()
-    expect(instance.state.status).toBe('mabar')
-});
