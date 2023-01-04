71aabf4ae41c67920ae2b3fb8944b1ddc49a4dd6
HUAWEI <mabar@tut.by>
2023-01-04 14:38:30 +0300

    Updates

---
+++ static/js/120.2ca2338d.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkapp_lesson=self.webpackChunkapp_lesson||[]).push([[120],{7562:function(e,s,n){n.r(s);var u=n(184);s.default=function(){return(0,u.jsx)("div",{children:"News"})}}}]);
+//# sourceMappingURL=120.2ca2338d.chunk.js.map
